f4ae1653a19b6f19becf154f8bd7910d7097da54
alnah <alexis.nahan@gmail.com>
2025-12-26 21:53:13 -0300

    refactor: update minimal CLI in main.go to convert from html to pdf

--- main.go
+++ main.go
@@ -7,24 +7,48 @@ import (
 
 // Exit codes and CLI argument positions.
 const (
-	exitSuccess       = 0
-	exitError         = 1
-	minRequiredArgs   = 2
-	inputFileArgIndex = 1
+	exitSuccess        = 0
+	exitError          = 1
+	minRequiredArgs    = 3
+	inputFileArgIndex  = 1
+	outputFileArgIndex = 2
+	cssFileArgIndex    = 3
 )
 
 func main() {
 	if len(os.Args) < minRequiredArgs {
-		fmt.Fprintln(os.Stderr, "usage: md2pdf <file.md>")
+		fmt.Fprintln(os.Stderr, "usage: md2pdf <input.md> <output.pdf> [style.css]")
 		os.Exit(exitError)
 	}
 
-	converter := NewPandocConverter()
-	html, err := converter.ToHTML(os.Args[inputFileArgIndex])
+	inputPath := os.Args[inputFileArgIndex]
+	outputPath := os.Args[outputFileArgIndex]
+
+	// Convert Markdown to HTML
+	pandoc := NewPandocConverter()
+	html, err := pandoc.ToHTML(inputPath)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(exitError)
 	}
 
-	fmt.Print(html)
+	// Read optional CSS file
+	var cssContent string
+	if len(os.Args) > cssFileArgIndex {
+		cssBytes, err := os.ReadFile(os.Args[cssFileArgIndex])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(exitError)
+		}
+		cssContent = string(cssBytes)
+	}
+
+	// Convert HTML to PDF
+	chrome := NewChromeConverter()
+	if err := chrome.ToPDF(html, cssContent, outputPath); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(exitError)
+	}
+
+	fmt.Printf("Created %s\n", outputPath)
 }
