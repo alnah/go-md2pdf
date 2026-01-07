530bbae167c05a67f50f4f61f0f494c87b51c224
alnah <alexis.nahan@gmail.com>
2026-01-07 13:07:57 -0300

    feat: wire service pool in main with automaxprocs

--- cmd/md2pdf/main.go
+++ cmd/md2pdf/main.go
@@ -4,17 +4,44 @@ import (
 	"fmt"
 	"os"
 
-	md2pdf "github.com/alnah/go-md2pdf"
+	"go.uber.org/automaxprocs/maxprocs"
 )
 
 // Version is set at build time via ldflags.
 var Version = "dev"
 
 func main() {
-	svc := md2pdf.New()
-	defer svc.Close()
+	// Parse flags first to get workers count and verbose
+	flags, _, err := parseFlags(os.Args)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
-	if err := run(os.Args, svc); err != nil {
+	// Configure GOMAXPROCS with conditional logging
+	// Error ignored: maxprocs.Set only fails if GOMAXPROCS env is invalid,
+	// in which case Go runtime defaults apply and the program continues safely.
+	if flags.verbose {
+		_, _ = maxprocs.Set(maxprocs.Logger(func(format string, args ...interface{}) {
+			fmt.Fprintf(os.Stderr, format+"\n", args...)
+		}))
+	} else {
+		_, _ = maxprocs.Set(maxprocs.Logger(func(string, ...interface{}) {}))
+	}
+
+	// Create pool with resolved size
+	poolSize := resolvePoolSize(flags.workers)
+	if flags.verbose {
+		fmt.Fprintf(os.Stderr, "Pool size: %d\n", poolSize)
+	}
+	pool := NewServicePool(poolSize)
+	defer pool.Close()
+
+	if flags.verbose {
+		fmt.Fprintln(os.Stderr, "Starting conversion...")
+	}
+
+	if err := run(os.Args, pool); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
